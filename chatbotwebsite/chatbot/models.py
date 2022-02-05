3b6d8f8cabb44a2159b725bfe31180da621e4496
Om Rawal <omrawal007@gmail.com>
2022-02-05 20:15:00 +0530

    created database and superuser

--- chatbotwebsite/chatbot/models.py
+++ chatbotwebsite/chatbot/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class ChatbotUser(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    age = models.PositiveIntegerField()
+    gender = models.BooleanField()  # True = male ; False = Female
+    isStudent = models.BooleanField()
+
+
+class UserScore(models.Model):
+    owner = models.ForeignKey(ChatbotUser, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    updatedAt = models.DateTimeField(auto_now=True)
